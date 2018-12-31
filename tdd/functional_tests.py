a2348382b8fb76e38ab8191aed6d5b7c0807da4c
Richard Lee <mt.richard.lee@gmail.com>
2019-01-01 02:29:42 +1100

    end of chapter 2 for tdd... unittest operational.

--- tdd/functional_tests.py
+++ tdd/functional_tests.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
+import unittest
 import os
 
-gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), 'geckodriver.exe'))
-browser = webdriver.Firefox(executable_path = gecko)
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), 'geckodriver.exe'))
+        self.browser = webdriver.Firefox(executable_path = gecko)
+        self.browser.implicitly_wait(3)
+      
+    def tearDown(self):     
+        self.browser.quit()
+  
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')     
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+  
+if __name__ == '__main__':
+    unittest.main()  
