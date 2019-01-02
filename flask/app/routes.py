42a4a7b0c055e6e36d853d5df67ad0122149dbca
Richard Lee <mt.richard.lee@gmail.com>
2019-01-03 01:59:45 +1100

    flask - halfway through Chapter 6, may need some corrections

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +18,22 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+    
+# @app.route('/user/<username>')
+# @login_required
+# def user(username):
+    # user = User.query.filter_by(username=username).first_or_404()
+    # posts = [
+        # {'author': user, 'body': 'Test post #1'},
+        # {'author': user, 'body': 'Test post #2'}
+    # ]
+    # return render_template('user.html', user=user, posts=posts)
--- flask/microblog.py
+++ flask/microblog.py
@@ -1,1 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+# Flask Shell needs "set FLASK_APP=microblog.py"
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
