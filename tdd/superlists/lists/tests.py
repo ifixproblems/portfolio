65f4180128fb6eedbed9e68990cc625862ad6749
Richard Lee <mt.richard.lee@gmail.com>
2019-01-04 05:32:52 +1100

    added some tdd code

--- tdd/superlists/lists/tests.py
+++ tdd/superlists/lists/tests.py
@@ -4,6 +4,10 @@ from __future__ import unicode_literals
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
+from django.http import HttpRequest
+
+from views import home_page
+#from list.views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -12,3 +16,11 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
         
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+      
+      
