239912ce8c712f6cda18da252b765fad2022495c
Richard Lee <mt.richard.lee@gmail.com>
2019-01-12 06:28:34 +1100

    have to add tests and repair flask

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -2,11 +2,14 @@ import flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = flask.Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login' # maps to url_for() for login
 
 from app import routes, models
 
--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,10 +1,14 @@
 from flask import render_template, flash, redirect
-from app import app
+from app import app, db
+from app.forms import RegistrationForm
 from app.forms import LoginForm
-
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
 
 @app.route('/')
 @app.route('/index')
+@login_required # Decorator forces login
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -17,23 +21,54 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template("index.html", title='Home Page', posts=posts)
     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
     
-# @app.route('/user/<username>')
-# @login_required
-# def user(username):
-    # user = User.query.filter_by(username=username).first_or_404()
-    # posts = [
-        # {'author': user, 'body': 'Test post #1'},
-        # {'author': user, 'body': 'Test post #2'}
-    # ]
-    # return render_template('user.html', user=user, posts=posts)
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+    
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+    
+from app.forms import EditProfileForm
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)    
