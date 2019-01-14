9e35f62bcdab5fa1e167bdb02c6acf44e1217ea8
Richard Lee <mt.richard.lee@gmail.com>
2019-01-14 19:55:03 +1100

    remove flask api item

--- flask/microblog.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from app.models import User, Post
-
-
-# Flask Shell needs "set FLASK_APP=microblog.py"
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
-
